c415c7190f587062dd1661f11d44073c9f432b60
Deepak Kumar Das <dpk.ds3@gmail.com>
2018-05-27 15:49:10 +0530

    Update views.py
    changes for the code to behave for obvious inputs :)

--- proj_fibonacci/calc_fibonacci/views.py
+++ proj_fibonacci/calc_fibonacci/views.py
@@ -7,10 +7,13 @@ def index(request):
     return render(request,'calc_fibonacci/index.html')
 
 def result(request):
+    start_time = time.time()
     if request.method == 'POST':
         input = int(request.POST.get('input'))
-        start_time = time.time()
-        output = mat_mul(mat_power(A,input-1),v)[0]
+        if input == 1:
+            output = 1
+        else:
+            output = mat_mul(mat_power(A,input-1),v)[0]
         total_time = time.time()-start_time
-        total_time = '{:.20f}'.format(total_time)
+        total_time = '{:.6f}'.format(total_time)
     return render(request,'calc_fibonacci/results.html',{'output':output,'time':total_time})
